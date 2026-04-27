5431166d2b7a5a6941f0edb642a27c5b77513416
Aditya Nishad <146601169+adixyc@users.noreply.github.com>
2026-04-27 11:01:48 +0530

    Update pricing details and fix function names

--- ub.py
+++ ub.py
@@ -89,22 +89,23 @@ async def auto_price(event):
             replied_users.add(user_id)
             await asyncio.sleep(2)
 
-            await event.respond("""    🐣🦋 𝗡𝗔𝗩𝗬𝗔 𝗔𝗩𝗔𝗜𝗟𝗔𝗕𝗟𝗘 🐣🦋
+            await event.respond("""
+                🐣🦋 𝗡𝗔𝗩𝗬𝗔 𝗔𝗩𝗔𝗜𝗟𝗔𝗕𝗟𝗘 🐣🦋
 
        🍒  𝗩𝗢𝗜𝗖𝗘 𝗖𝗔𝗟𝗟  🍒
 
-🍒𝟱 𝗠𝗜𝗡𝗨𝗧𝗘𝗦 - 100 𝗥𝗦 💦
-🍒𝟭𝟬 𝗠𝗜𝗡𝗨𝗧𝗘𝗦 - 250 𝗥𝗦 💦
+🍒𝟱 𝗠𝗜𝗡𝗨𝗧𝗘𝗦 - 𝟮𝟬𝟬 𝗥𝗦 💦
+🍒𝟭𝟬 𝗠𝗜𝗡𝗨𝗧𝗘𝗦 - 𝟯𝟱𝟬 𝗥𝗦 💦
 
        🎀 𝗩𝗜𝗗𝗘𝗢 𝗖𝗔𝗟𝗟 💘
 
-🍒𝟱 𝗠𝗜𝗡𝗨𝗧𝗘𝗦 - 3𝟬𝟬 𝗥𝗦  💦
-🍒𝟭𝟬 𝗠𝗜𝗡𝗨𝗧𝗘𝗦 - 6𝟬𝟬 𝗥𝗦 💦
+🍒𝟱 𝗠𝗜𝗡𝗨𝗧𝗘𝗦 - 𝟱𝟬𝟬 𝗥𝗦  💦
+🍒𝟭𝟬 𝗠𝗜𝗡𝗨𝗧𝗘𝗦 - 𝟴𝟬𝟬 𝗥𝗦 💦
 
        🌟 𝗦𝗘𝗫 𝗖𝗛𝗔𝗧 👄
 
-🍒𝟱 𝗠𝗜𝗡𝗨𝗧𝗘𝗦 - 1𝟬𝟬 𝗥𝗦 💦
-🍒𝟭𝟬 𝗠𝗜𝗡𝗨𝗧𝗘𝗦 - 200 𝗥𝗦 💦
+🍒𝟱 𝗠𝗜𝗡𝗨𝗧𝗘𝗦 - 𝟮𝟬𝟬 𝗥𝗦 💦
+🍒𝟭𝟬 𝗠𝗜𝗡𝗨𝗧𝗘𝗦 - 𝟯𝟱𝟬 𝗥𝗦 💦
 
        🎀 𝗦𝗣𝗘𝗖𝗜𝗔𝗟 𝗦𝗛𝗢𝗪 💘
 
@@ -140,7 +141,7 @@ async def send_price_list(event):
 #------
 
 @client.on(events.NewMessage(outgoing=True, pattern=r"\.fl"))
-async def price_list(event):
+async def price_lixst(event):
     text = """
     🐣🦋 𝗡𝗔𝗩𝗬𝗔 𝗔𝗩𝗔𝗜𝗟𝗔𝗕𝗟𝗘 🐣🦋
 
@@ -193,7 +194,7 @@ async def welcome_new_user(event):
 #-----
 
 @client.on(events.NewMessage(outgoing=True, pattern=r"\.dm"))
-async def price_list(event):
+async def price_lssist(event):
     text = """
 DM @niximia to buy Telegram/WhatsApp accounts.
 """
@@ -201,7 +202,7 @@ DM @niximia to buy Telegram/WhatsApp accounts.
 
 
 @client.on(events.NewMessage(outgoing=True, pattern=r"\.rl"))
-async def price_list(event):
+async def price_lsisst(event):
     text = """
     🐣🦋 𝗡𝗔𝗩𝗬𝗔 𝗔𝗩𝗔𝗜𝗟𝗔𝗕𝗟𝗘 🐣🦋
 
